8f3859f0ad0962f22cbfcc594d14f2bed4607506
joninoni <jonibeltranhayer@gmail.com>
2023-12-03 10:56:49 -0600

    creacion de las funciones para mostrar el curso seleccionado y para borrar

--- js/copia2.js
+++ js/copia2.js
@@ -3,7 +3,7 @@ const carrito=document.querySelector("#carrito");
 const contenedorCarrito=document.querySelector("#lista-carrito tbody");
 const vaciarCarritoBtn=document.querySelector("#vaciar-carrito");
 const listaCursos=document.querySelector("#lista-cursos")
-console.log(listaCursos);
+let carritoCursos=[];
 // Eventos
 cargarEventos();
 function cargarEventos(){
@@ -25,5 +25,18 @@ function leerCurso(curso){
         id:curso.querySelector('a').getAttribute('data-id'),
         cantidad:1,
     }
-    console.log(objCurso);
+    carritoCursos=[...carritoCursos,objCurso];
+    carritoHtml();
+}
+//funcion que genera el html y lo muestra
+function carritoHtml(){
+    carritoCursos.forEach(curso =>{
+        const row =document.createElement('tr');
+        row.innerHTML=`<td><img src=${curso.img} width="100"></td>
+                       <td>${curso.titulo}</td>
+                       <td>${curso.precio}</td>
+                       <td>${curso.cantidad}</td>
+                       `                       
+        contenedorCarrito.appendChild(row);
+    })
 }
